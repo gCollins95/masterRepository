c263c4398f9b38069a595490a078d78f46b54d23
gCollins95 <gcollins192@gmail.com>
2013-12-05 18:21:50 -0500

    This time?

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var app = express();
+var Twit = require('twit');
 
-  var output = "Hello World";
-app.get('/', function(req, res){
-  res.send(output);
+var T = new Twit({
+    consumer_key:         'eXv3EVWqJi7IH1942cJw',
+    consumer_secret:      'QX33rqAHzzlQH07XecLPk0iextogXbuZRUsJT16U',
+    access_token:         '2223734401-XWpRuEtNnEjOfdzafA9mZEYgMgEgh3Qov8s0746',
+    access_token_secret:  'hMTTRs053GBv7CapaEQEuWdGn1wBFzIkaVR0FRMfkagQh'
+});
+
+
+T.get('search/tweets', { q: 'banana since:2011-11-11', count: 5 }, function(err, reply) {
+  console.log(reply);
+});
+
+
+
+app.get('/', function(req, res) {
+   res.sendfile('./Homepage.html');
 });
 
 var port = process.env.PORT || 5000;
